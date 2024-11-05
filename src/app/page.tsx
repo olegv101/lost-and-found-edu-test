745e11677547c4006f230a637d5559cff49fc7d2
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-11-05 07:45:28 -0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
             />
           </div>
           <h1 className="text-5xl md:text-6xl font-bold text-center mb-6">
-            Ryder's Cal Lost & Found
+            Cal Lost & Found
           </h1>
           <p className="text-xl md:text-2xl text-center mb-8 max-w-2xl text-[#FDB515]">
-            Reconnecting Golden Bears with their belongings
+            Reconnecting Golden Bears with their belongings without snapchat
           </p>
         </div>
       </div>
